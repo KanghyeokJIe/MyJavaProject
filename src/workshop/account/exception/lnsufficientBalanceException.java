3fa8b1b0f475852747358d325bc0e8d2f2ffd5f9
kanghyeokjie <kanghyeokjie@gmail.com>
2025-04-25 09:30:36 +0900

    new update

---
+++ src/workshop/account/exception/lnsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package workshop.account.exception;
+
+public class InsufficientBalanceException extends Exception {
+	//private String errMessage;
+	public InsufficientBalanceException(String errMessage) {
+		//this.errMessage = errMessage;
+		super(errMessage);
+	}
+}
